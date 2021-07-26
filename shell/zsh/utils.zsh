ce02722ca98b60d95d14335f04e9e2c2234c79af
Ting Zhou <ztlevi.work@gmail.com>
2021-07-26 21:13:42 +0800

    misc

--- shell/zsh/utils.zsh
+++ shell/zsh/utils.zsh
@@ -97,7 +97,7 @@ function update_my_repos() {
   (
     cd $DOTTY_HOME
     git submodule update --init config
-    [[ -d $DOTTY_ASSETS_HOME ]] && git submodule update --init assets
+    [[ -d ${DOTTY_ASSETS_HOME}/fonts ]] && git submodule update --init assets
     update_topics &>/dev/null
   )
 
