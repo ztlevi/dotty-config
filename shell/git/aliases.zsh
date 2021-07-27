f8517981f882fc091535da45d67fc51fed8e961b
Ting Zhou <ztlevi.work@gmail.com>
2021-07-27 11:47:34 +0800

    shell/git: add git-rename-master-to-main

--- shell/git/aliases.zsh
+++ shell/git/aliases.zsh
@@ -11,3 +11,12 @@ alias git='noglob git'
 alias g="git"
 alias gci="gh pr checks"
 alias gcpr="gh pr create --assignee ztlevi"
+
+# Use after you change your branch name from master to main
+# https://github.com/<user>/<project-name>/settings/branches
+function git-rename-master-to-main() {
+  git branch -m master main
+  git fetch origin
+  git branch -u origin/main main
+  git remote set-head origin -a
+}
