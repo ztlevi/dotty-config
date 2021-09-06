c6d24a9f24cf7abe99ec52b90b9964cb4239673f
Ting Zhou <ztlevi.work@gmail.com>
2021-09-06 11:35:15 -0700

    wm/bspwm: move files

--- wm/bspwm/rc.zsh
+++ wm/bspwm/rc.zsh
@@ -1,3 +1,3 @@
 source $DOTTY_CONFIG_HOME/wm/general/rc.zsh
 
-alias polybar-start="$DOTTY_CONFIG_HOME/wm/bspwm/config/polybar/run.sh"
+alias polybar-start="$DOTTY_CONFIG_HOME/desktop/polybar/run.sh"
