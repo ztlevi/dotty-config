a615dae36b56e71a42b2588226d2848b4fb2ea4b
Ting Zhou <ztlevi.work@gmail.com>
2021-09-07 01:47:47 -0700

    misc/chinese: fix script

--- misc/chinese/install_fcitx_config.sh
+++ misc/chinese/install_fcitx_config.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env zsh
-mkdir -p ${XDG_CONFIG_HOME}/fcitx
 
 # 1. System input method
-mklink $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/config ${XDG_CONFIG_HOME}/fcitx/config
-mklink $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/rime/* ${XDG_CONFIG_HOME}/fcitx/rime/
-[[ -d ${DOTTY_ASSETS_HOME}/rime-dictionaries ]] && mklink ${DOTTY_ASSETS_HOME}/rime-dictionaries/*.dict.yaml ${XDG_CONFIG_HOME}/fcitx/rime/
-mklink pam_environment ${HOME}/.pam_environment
+mkdir -p $XDG_CONFIG_HOME/fcitx/rime
+
+ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/config $XDG_CONFIG_HOME/fcitx/config
+ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/rime/* $XDG_CONFIG_HOME/fcitx/rime/
+ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/pam_environment ${HOME}/.pam_environment
+
+[[ -d ${DOTTY_ASSETS_HOME}/rime-dictionaries ]] && ln -s -f $DOTTY_ASSETS_HOME/rime-dictionaries/*.dict.yaml $XDG_CONFIG_HOME/fcitx/rime/
 
 case $(_os) in
   macos)
-    mklink ~/.config/fcitx/rime ~/Library/Rime
+    ln -s -f ~/.config/fcitx/rime ~/Library/Rime
     ;;
 esac
 
 # 2. Emacs input method
 
-# Don't use ${XDG_CONFIG_HOME}/fcitx/rime as your emacs fcitx config dir. It will cause conflict.
-mklink $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/rime/* $XDG_CONFIG_HOME/fcitx/emacs-rime/
-[[ -d ${DOTTY_ASSETS_HOME}/rime-dictionaries ]] && mklink ${DOTTY_ASSETS_HOME}/rime-dictionaries/*.dict.yaml $XDG_CONFIG_HOME/fcitx/emacs-rime/
+# Don't use $XDG_CONFIG_HOME/fcitx/rime as your emacs fcitx config dir. It will cause conflict.
+mkdir -p $XDG_CONFIG_HOME/fcitx/emacs-rime/
+ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/rime/* $XDG_CONFIG_HOME/fcitx/emacs-rime/
+[[ -d $DOTTY_ASSETS_HOME/rime-dictionaries ]] && ln -s -f $DOTTY_ASSETS_HOME/rime-dictionaries/*.dict.yaml $XDG_CONFIG_HOME/fcitx/emacs-rime/
 
-local link_files=("opencc" "emoji_suggestion.yaml" "zhwiki.dict.yaml")
-for file in ${link_files[@]}; do
-  mklink ${XDG_CONFIG_HOME}/fcitx/rime/$file $XDG_CONFIG_HOME/fcitx/emacs-rime/
+link_files=("opencc" "emoji_suggestion.yaml" "zhwiki.dict.yaml")
+for file in ''${link_files[@]}; do
+  ln -s -f $XDG_CONFIG_HOME/fcitx/rime/$file $XDG_CONFIG_HOME/fcitx/emacs-rime/
 done
 
 # Download zhwiki pinyin dictionary
 if [[ ! -f $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml ]]; then
   [[ ! -f $WGETRC ]] && touch $WGETRC
-  wget --no-check-certificate $(get_github_latest_release_url felixonmars/fcitx5-pinyin-zhwiki "\.dict\.yaml") \
-    -O $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml
+  wget -c https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.1/zhwiki-20200601.dict.yaml \
+    -O $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml || echo "Download zhwiki failed"
 fi
