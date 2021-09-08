0817ca1ce8bf3526ff759843b8aa172b78090e53
Ting Zhou <ztlevi.work@gmail.com>
2021-09-08 00:26:22 -0700

    misc/chinese: update install script

--- misc/chinese/install_fcitx_config.sh
+++ misc/chinese/install_fcitx_config.sh
@@ -2,6 +2,8 @@
 
 # rm -rf $XDG_CONFIG_HOME/fcitx $XDG_CONFIG_HOME/fcitx5 $XDG_DATA_HOME/fcitx5
 
+mkdir -p $XDG_CONFIG_HOME/fcitx/rime
+
 # Download zhwiki pinyin dictionary
 if [[ ! -f $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml ]]; then
   [[ ! -f $WGETRC ]] && touch $WGETRC
@@ -9,19 +11,10 @@ if [[ ! -f $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml ]]; then
     -O $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml
 fi
 
-# Install emoji
-mkdir -p /tmp/fcitx-install
-(
-  cd /tmp/fcitx-install
-  curl -fsSL https://git.io/rime-install | bash -s -- emoji
-)
-rm -rf /tmp/fcitx-install
-
 # 1. System input method
-mkdir -p $XDG_CONFIG_HOME/fcitx/rime
 
 ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/config $XDG_CONFIG_HOME/fcitx/config
-ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/rime/* $XDG_CONFIG_HOME/fcitx/rime/
+ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/rime/* $XDG_CONFIG_HOME/fcitx/rime/
 ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/.pam_environment ${HOME}/.pam_environment
 
 [[ -d ${DOTTY_ASSETS_HOME}/rime-dictionaries ]] && ln -s -f $DOTTY_ASSETS_HOME/rime-dictionaries/*.dict.yaml $XDG_CONFIG_HOME/fcitx/rime/
@@ -36,10 +29,7 @@ esac
 
 # Don't use $XDG_CONFIG_HOME/fcitx/rime as your emacs fcitx config dir. It will cause conflict.
 mkdir -p $XDG_CONFIG_HOME/fcitx/emacs-rime/
-ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/fcitx/rime/* $XDG_CONFIG_HOME/fcitx/emacs-rime/
+ln -s -f $DOTTY_CONFIG_HOME/misc/chinese/config/rime/* $XDG_CONFIG_HOME/fcitx/emacs-rime/
 [[ -d $DOTTY_ASSETS_HOME/rime-dictionaries ]] && ln -s -f $DOTTY_ASSETS_HOME/rime-dictionaries/*.dict.yaml $XDG_CONFIG_HOME/fcitx/emacs-rime/
 
-link_files=("opencc" "emoji_suggestion.yaml" "zhwiki.dict.yaml")
-for file in ''${link_files[@]}; do
-  ln -s -f $XDG_CONFIG_HOME/fcitx/rime/$file $XDG_CONFIG_HOME/fcitx/emacs-rime/
-done
+ln -s -f $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml $XDG_CONFIG_HOME/fcitx/emacs-rime/
