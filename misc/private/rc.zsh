87e8058ee7afbde9fd98dc64687e2c3a7731ccb9
Ting Zhou <ztlevi.work@gmail.com>
2021-10-22 18:26:37 -0700

    misc/private: use yt-dlp

--- misc/private/rc.zsh
+++ misc/private/rc.zsh
@@ -1,13 +1,13 @@
-zinit wait lucid light-mode for soimort/you-get
+# zinit wait lucid light-mode for soimort/you-get
+# alias yg='you-get'
 
 if _is_callable reveal-md; then
   alias rv='reveal-md --highlight-theme github --theme white --watch'
   alias rvs='reveal-md --highlight-theme github --theme white --static revealjs_output'
 fi
 
-alias yg='you-get'
-if _is_callable youtube-dl; then
-  alias yd='youtube-dl --write-auto-sub --ignore-errors'
-  alias yd2mp4='youtube-dl --write-auto-sub --ignore-errors --recode-video mp4'
-  alias yda='youtube-dl -i -o "%(title)s.%(ext)s" --embed-thumbnail --extract-audio --audio-format mp3 --audio-quality 0'
+if _is_callable yt-dlp; then
+  alias yd='yt-dlp --write-auto-sub --ignore-errors'
+  alias yd2mp4='yt-dlp --write-auto-sub --ignore-errors --recode-video mp4'
+  alias yda='yt-dlp -i -o "%(title)s.%(ext)s" --embed-thumbnail --extract-audio --audio-format mp3 --audio-quality 0'
 fi
