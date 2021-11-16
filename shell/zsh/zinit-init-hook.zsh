137ada39e4f09e841134a25b81f141a84c363e82
Ting Zhou <ztlevi.work@gmail.com>
2021-11-15 19:26:16 -0800

    shell/zsh: update zinit git url

--- shell/zsh/zinit-init-hook.zsh
+++ shell/zsh/zinit-init-hook.zsh
@@ -7,7 +7,7 @@
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
   print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
   mkdir -p "$HOME/.zinit" && chmod g-rwX "$HOME/.zinit"
-  git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" &&
+  git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin &&
     print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
 
