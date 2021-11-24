a45dddb6a21656167c11fcac12a1228d45f4e329
Ting Zhou <ztlevi.work@gmail.com>
2021-11-23 18:17:54 -0800

    shell/zsh: fix annex url

--- shell/zsh/zinit-init-hook.zsh
+++ shell/zsh/zinit-init-hook.zsh
@@ -16,7 +16,7 @@ autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
 # Note: Use zinit-annex-meta-plugins to install console seems causing issue on Nixos.
-zinit light-mode for zinit-annex-meta-plugins
+zinit light-mode for zdharma-continuum/zinit-annex-meta-plugins
 # Load a few important annexes, without Turbo (this is currently required for annexes)
 zinit for annexes
 # This loads plugin groups defined in zinit-zsh/z-a-meta-plugins
