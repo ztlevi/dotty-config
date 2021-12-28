504d461e02fb2727f54f4168e7b18adf56099af0
Ting Zhou <ztlevi.work@gmail.com>
2021-12-28 00:06:29 -0800

    dev/cc: add cmake alias

--- dev/cc/rc.zsh
+++ dev/cc/rc.zsh
@@ -2,3 +2,11 @@ if _is_callable clang-format; then
   # alias format-all-clang="find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)' -exec clang-format -style=file -i {} \;"
   alias format-all-clang="fd -x clang-format -i -style=file {} \; -e c -e cc -e cpp -e h -e hh -e hpp ."
 fi
+
+function cmake-pre-build () {
+  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -S . -B build
+  ln -s -f build/compile_commands.json
+}
+function cmake-build-all() {
+  cmake --build build --config Debug --target all -j 14 --
+}
