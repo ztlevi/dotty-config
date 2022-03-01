389e4e30bf0d5e788a27b395a8b5629fbb0bba5a
Ting Zhou <ztlevi.work@gmail.com>
2022-03-01 02:21:05 -0800

    shell/sk: update color

--- shell/sk/env.zsh
+++ shell/sk/env.zsh
@@ -4,5 +4,5 @@ fi
 
 export SKIM_DEFAULT_COMMAND="fd --type f || git ls-tree -r --name-only HEAD || rg --files || find ."
 export SKIM_CTRL_T_COMMAND=$SKIM_DEFAULT_COMMAND
-local skim_theme_options="--color=light,current:7,current_bg:4,current_match_bg:3,current_match:0,info:4"
+local skim_theme_options="--color=current:15,current_bg:12,current_match_bg:3,current_match:0,info:4"
 export SKIM_DEFAULT_OPTIONS="--reverse --ansi ${skim_theme_options}"
