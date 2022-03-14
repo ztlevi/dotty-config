fa4b2d6c37c1a6a638e73561d47cbcb265abefcb
Ting Zhou <ztlevi.work@gmail.com>
2022-03-14 15:27:10 -0700

    dev/cc: add release alias

--- dev/cc/rc.zsh
+++ dev/cc/rc.zsh
@@ -3,7 +3,7 @@ if _is_callable clang-format; then
   alias format-all-clang="fd -x clang-format -i -style=file {} \; -e c -e cc -e cpp -e h -e hh -e hpp ."
 fi
 
-function cmake-configure () {
+function cmake-configure-debug () {
   cmake --no-warn-unused-cli \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
@@ -15,7 +15,18 @@ function cmake-configure () {
 function cmake-build() {
   cmake --build build --config Debug --target all -j 14 --
 }
-alias cbb="cmake-configure && cmake-build"
+function cmake-configure-release () {
+  cmake --no-warn-unused-cli \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
+    -S . -B release -G Ninja
+}
+function cmake-release () {
+  cmake --build release --config Release --target all -j 14 --
+}
+alias cbb="cmake-configure-debug && cmake-build"
+alias crr="cmake-configure-release && cmake-release"
 function ctest-all() {
   ctest -j14 -C Debug -T test --output-on-failure
 }
