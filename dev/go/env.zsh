77107134dd8f0b197eafe0a7a6bcc5286b904910
Ting Zhou <ztlevi.work@gmail.com>
2022-04-08 17:28:37 -0700

    dev/go: fix env

--- dev/go/env.zsh
+++ dev/go/env.zsh
@@ -1,5 +1,6 @@
 export GOPATH="$XDG_DATA_HOME/go"
-export GOROOT=$(go env GOROOT)
+_is_callable go && export GOROOT=$(go env GOROOT)
+# go env -w GOPROXY=direct
 
 # Adds $GOPATH/bin's to PATH
 path=( `printf '%s/bin\n' ${(@s/:/)GOPATH}` $path )
