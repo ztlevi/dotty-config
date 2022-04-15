e0cbe189d18eb5d5be3e98f3a58766d4b25ff1be
Ting Zhou <ztlevi.work@gmail.com>
2022-04-15 13:08:45 -0700

    shell/zsh: fix prg

--- shell/zsh/rc.zsh
+++ shell/zsh/rc.zsh
@@ -57,10 +57,7 @@ alias cssh='$EDITOR $HOME/.ssh/config'
 
 alias ag="noglob ag -p $XDG_CONFIG_HOME/ag/agignore"
 alias rg='noglob rg'
-if _is_callable procs; then
-  alias ps="procs"
-fi
-alias prg="/bin/ps aux | rg -i"
+function prg() {ps aux | rg -i $@}
 function grep_search() { echo $2 | grep -qiP $1; }
 function rg_search() { echo $2 | rg -qS $1; }
 function vread() {
