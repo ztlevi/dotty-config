39351768a4579af3f95249398f77d14b2b5792cd
Ting Zhou <ztlevi.work@gmail.com>
2022-05-02 15:10:56 -0700

    dev/go: update go env

--- dev/go/env.zsh
+++ dev/go/env.zsh
@@ -1,6 +1,8 @@
-export GOPATH="$XDG_DATA_HOME/go"
-_is_callable go && export GOROOT=$(go env GOROOT)
-# go env -w GOPROXY=direct
+if _is_callable go; then
+  go env -w GOPATH "$XDG_DATA_HOME/go"
+  _is_callable go && go env -w GOROOT "$(go env GOROOT)"
+  # go env -w GOPROXY=direct
 
-# Adds $GOPATH/bin's to PATH
-path=( `printf '%s/bin\n' ${(@s/:/)GOPATH}` $path )
+  # Adds $GOPATH/bin's to PATH
+  path=( `printf '%s/bin\n' ${(@s/:/)GOPATH}` $path )
+fi
