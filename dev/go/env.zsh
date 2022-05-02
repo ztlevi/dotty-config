bfa081430f6faf5f9af41db9cc620fe6efaae476
Ting Zhou <ztlevi.work@gmail.com>
2022-05-02 22:12:33 +0000

    dev/go: fix env

--- dev/go/env.zsh
+++ dev/go/env.zsh
@@ -1,6 +1,6 @@
 if _is_callable go; then
-  go env -w GOPATH "$XDG_DATA_HOME/go"
-  _is_callable go && go env -w GOROOT "$(go env GOROOT)"
+  go env -w GOPATH="$XDG_DATA_HOME/go"
+  go env -w GOROOT="$(go env GOROOT)"
   # go env -w GOPROXY=direct
 
   # Adds $GOPATH/bin's to PATH
