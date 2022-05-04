cd5c6ac51facfdd747e772ee225a97ccdaca4c1f
Ting Zhou <ztlevi.work@gmail.com>
2022-05-04 15:41:52 -0700

    shell/joshuto: update key

--- shell/joshuto/rc.zsh
+++ shell/joshuto/rc.zsh
@@ -1,7 +1,27 @@
 function joshuto-cd() {
-  joshuto --lastdir /tmp/joshuto-lastdir
-  LASTDIR="$(cat /tmp/joshuto-lastdir)"
-  [ "$LASTDIR" != "$(pwd)" ] && cd "$LASTDIR"
+  ID="$$"
+  mkdir -p /tmp/$USER
+  OUTPUT_FILE="/tmp/$USER/joshuto-cwd-$ID"
+  env joshuto --output-file "$OUTPUT_FILE" $@
+  exit_code=$?
+
+  case "$exit_code" in
+    # regular exit
+    0)
+      ;;
+    # output contains current directory
+    101)
+      JOSHUTO_CWD=$(cat "$OUTPUT_FILE")
+      cd "$JOSHUTO_CWD"
+      ;;
+    # output selected files
+    102)
+      cat "$OUTPUT_FILE"
+      ;;
+    *)
+      echo "Exit code: $exit_code"
+      ;;
+  esac
 }
 
 function widget-joshuto-cd() {
