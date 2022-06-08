e4b3be7c7be976c991265fcd5230ae8d33ee74fd
Ting Zhou <ztlevi.work@gmail.com>
2022-06-07 22:57:05 -0700

    misc/unison: update unison-sync

--- misc/unison/rc.zsh
+++ misc/unison/rc.zsh
@@ -1,12 +1,21 @@
 function unison-sync() {
   # Usage:
-  # unison-sync <path-to-your-local-workplace> ssh://<username>@<remote-hostname>/<remote-workplace>
-  # e.g. unison-sync /workplace ssh://ztlevi@kuro//workplace
+  # unison-sync <local-workplace> <username>@<remote-hostname> <remote-workplace>
+  # e.g. unison-sync /workplace ztlevi@kuro /workplace
   # You need to remove local or remote ~/.unison cache if failed
+  if [[ $# -ne 3 ]]; then
+    echo-fail "Need 3 argments but only have $#"
+    echo-info "Usage: unison-sync <local-workplace> <username>@<remote-hostname> <remote-workplace>"
+    return 0
+  fi
+  local remote_arg="ssh://$2/$3"
+
   if ! pgrep unison >/dev/null; then
     while :; do
+      echo "Kill remote unison process... " $(date)
+      ssh $2 "killall unison" || true
       echo "Starting unison process... " $(date)
-      unison -ui text default.prf $1 $2
+      unison -ui text default.prf $1 $remote_arg
       echo "Unison process exited.  Sleeping before restarting.  ^C to exit. " $(date)
       sleep 30
     done
