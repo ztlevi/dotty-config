84e49daee1157edc306bcf8a20d2e06a42c600d1
Ting Zhou <ztlevi.work@gmail.com>
2023-03-30 09:55:53 -0700

    dev/go: move go proxy

--- dev/go/rc.zsh
+++ dev/go/rc.zsh
@@ -4,7 +4,6 @@ zinit wait lucid for \
 if _is_callable go; then
   export GOPATH="$XDG_DATA_HOME/go"
   export GOROOT="$(brew --prefix go)/libexec"
-#   # go env -w GOPROXY=direct
 
   # Adds $GOPATH/bin's to PATH
   path=( `printf '%s/bin\n' ${(@s/:/)GOPATH}` $path )
