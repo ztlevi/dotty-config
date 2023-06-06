7b0a2803f431fa964022d93b850b2bd758b663f5
Ting Zhou <ztlevi.work@gmail.com>
2023-06-06 15:59:13 -0700

    misc/apps: add jetbrains bins

---
+++ misc/apps/env.zsh
@@ -0,0 +1,2 @@
+[[ -d "/Applications/IntelliJ IDEA.app/Contents/MacOS" ]] && path=($path "/Applications/IntelliJ IDEA.app/Contents/MacOS")
+[[ -d "/Applications/PyCharm.app/Contents/MacOS" ]] && path=($path "/Applications/PyCharm.app/Contents/MacOS")
