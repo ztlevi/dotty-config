dd6367b70254eb6933756566eb9969ff7dd7bc76
Ting Zhou <ztlevi.work@gmail.com>
2024-01-03 12:12:30 -0800

    misc/cspell: add cspell changed

--- misc/cspell/rc.zsh
+++ misc/cspell/rc.zsh
@@ -2,3 +2,4 @@
 
 alias cspell="cspell --config '$XDG_CONFIG_HOME/cspell/cspell.json'"
 alias cspell-all="cspell '**/*.{js,jsx,ts,tsx,c,cc,cpp,h,hh,hpp,go,json,py,java}'"
+alias cspell-changed="git diff --name-only HEAD | xargs -I{} cspell"
