a027445ce8809b136df3c8de7a2ceb1d1139c80c
Ting Zhou <ztlevi.work@gmail.com>
2025-05-15 13:46:19 -0700

    disable jenv

--- dev/java/rc.zsh
+++ dev/java/rc.zsh
@@ -1,5 +1,5 @@
 # version plugins cannot be loaded lazily
-zinit light-mode for OMZP::jenv
+# zinit light-mode for OMZP::jenv
 
 _is_callable google-java-format && alias format-all-java='google-java-format -i **/*.java'
 alias idea-format="'/Applications/IntelliJ IDEA.app/Contents/bin/format.sh'"
