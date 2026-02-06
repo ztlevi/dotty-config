c6d1b7636ff306a72cdbad45b482c5fb27c4abf9
Your Name <you@example.com>
2026-02-06 13:31:16 -0800

    misc/ai: update kiro cli

--- misc/ai/rc.zsh
+++ misc/ai/rc.zsh
@@ -1,2 +1,3 @@
 alias aw="aider --watch"
-alias kk="kiro-cli chat --model claude-opus-4.5"
+alias kk="kiro-cli chat --model claude-opus-4.6"
+alias kiro-enable-checkpoint="kiro-cli settings chat.enableCheckpoint true"
